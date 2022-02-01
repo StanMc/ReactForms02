f2080d0fcea54db9938bc23039a03192528462b2
McBarbar <42481948+StanMc@users.noreply.github.com>
2022-02-01 06:29:07 -0800

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("Hello");
+  const [buffer, setBuffer] = useState("What is your name?");
+
+  function handleFormChange(event) {
+    setBuffer(event.target.value);
+  }
+
+  function handleButtonClick() {
+    setName(buffer);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>{name} </h1>
+      <input
+        onChange={handleFormChange}
+        type="text"
+        placeholder="What's your name?"
+        value={buffer}
+      />
+      <button onClick={handleButtonClick}>Submit</button>
     </div>
   );
 }
